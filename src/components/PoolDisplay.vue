5826e15f90d4caaf17de3f8726cc654d2253ac57
Kristian Svanholm <KristianRorenSvanholm@gmail.com>
2023-02-22 12:06:34 +0100

    PoolDisplay

---
+++ src/components/PoolDisplay.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <v-file-input Solo Underlined></v-file-input>
+        <p>{{ pool }}</p>
+    </div>
+</template>
+
+
+<script>
+
+import { fetchPools } from '@/http/http';
+
+export default {
+    name: 'PoolDisplay',
+    
+    components: {
+    },
+    
+    data: () => ({
+        pool: []
+    }),
+
+    mounted: () => {
+        this.pool = fetchPools()
+    }
+};
+</script>
+  
