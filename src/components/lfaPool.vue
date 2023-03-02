130162c61f5f3ce0e127b0d1eca0b2584d5e5963
Kristian Svanholm <KristianRorenSvanholm@gmail.com>
2023-03-02 14:37:27 +0100

    New component lfaPool

---
+++ src/components/lfaPool.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <v-data-table
+            :headers="headers"
+            :items="pool"
+            item-key="name">
+        >
+        </v-data-table>
+    </div>
+</template>
+
+
+<script>
+
+export default {
+    name: 'lfaPool',
+    props: {pool: []},
+    computed:{
+        headers: function(){
+            return [
+                    {text: "Media Function", value:"name"},
+                    {text: "Time left", value:"duration"},
+                    {text: "Description", value:"description"}
+                ]
+        }
+    }
+}
+</script>
