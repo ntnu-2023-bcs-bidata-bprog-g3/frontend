dd79ddae4791b114953577f736f2f9b9accd4a57
Kristian Svanholm <KristianRorenSvanholm@gmail.com>
2023-03-02 17:45:07 +0100

    Add sublicense generation endpoint

--- src/http/http.js
+++ src/http/http.js
@@ -18,4 +18,6 @@ startApi();
 // Axios functions to call different APIS
 export const fetchPools = () => {return api.get('/pool/all')}
 export const fetchLfas = () => {return api.get('/lfa/licenses')}
+export const fetchLfaNames = () => {return api.get('/lfa')}
+export const generateSubLicense = (payload) => {return api.post('/license/lfa', payload)}
 export const uploadFile = (payload) => {return api.post('/license', payload, { headers: {"Content-Type": "multipart/form-data"} })}
