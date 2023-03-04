c7eea1f5e64e7fcd3ca07a28d5eaeb31c748d992
Kristian Svanholm <KristianRorenSvanholm@gmail.com>
2023-03-04 15:06:16 +0100

    Added consumption input and button on lfa license pools

--- src/components/lfaPool.vue
+++ src/components/lfaPool.vue
@@ -7,6 +7,25 @@
                 :headers="headers"
                 :items="pool"
                 item-key="name">
+                <template v-slot:[`item.actions`]="{ item }">
+                    <div>
+                        <v-icon
+                            :disabled="item.input<=0||item.input==undefined"
+                            @click="consume(item)"
+                            >
+                            mdi-delete
+                        </v-icon>
+                    </div>
+                </template>
+                <template v-slot:[`item.input`]="{item}">
+                    <v-text-field v-model="item.input"
+                        dense
+                        hide-details
+                        single-line
+                        label="Time in seconds"
+                        type="number"
+                    ></v-text-field>
+                </template>  
             </v-data-table>
         </v-card>
     </div>
@@ -18,12 +37,31 @@
 export default {
     name: 'lfaPool',
     props: {pool: [], name: String},
+    methods:{
+        consume(item){
+            this.$emit("consume", item.name, item.input)
+        }
+    },  
+    watch: {
+        pool(newPool) {
+            let items = JSON.parse(JSON.stringify(newPool))
+            items.map(item => {
+                if (!("input" in item)) {
+                    item.input = 0;
+                }
+                return item
+            })
+            newPool = items
+        }   
+    },
     computed:{
         headers: function(){
             return [
                     {text: "Media Function", value:"name"},
                     {text: "Time left", value:"duration"},
-                    {text: "Description", value:"description"}
+                    {text: "Description", value:"description"},
+                    {text: "Consume", value:"input"},
+                    {text: 'Actions', value: 'actions', sortable: false },
                 ]
         }
     }
